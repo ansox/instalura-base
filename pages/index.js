9b8a12613a26c6ad8f75c9a92ddcc7151572679d
Anso <anderson.floriano@gmail.com>
2022-02-04 14:26:46 -0300

    feat(pagehoc): create websitePageHOC

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,30 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Menu from '../src/components/commons';
-import Footer from '../src/components/commons/Footer';
-import Text from '../src/components/foundation/Text';
 import Button from '../src/components/commons/Button';
-import Grid from '../src/components/foundation/layout/Grid';
 import Box from '../src/components/foundation/layout/Box';
-import Modal from '../src/components/commons/Modal';
-import FormCadastro from '../src/components/patterns/FormCadastro';
+import Grid from '../src/components/foundation/layout/Grid';
+import Text from '../src/components/foundation/Text';
+import { WebsitePageContext } from '../src/components/wrappers/WebsitePage';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function Home() {
-  const [isModalOpen, setModalOpen] = React.useState(false);
+function HomeScreen() {
+  const websitePageContext = React.useContext(WebsitePageContext);
 
   return (
     <Box
-      flex="1"
       display="flex"
-      flexWrap="wrap"
       flexDirection="column"
-      justifyContent="space-between"
-      backgroundImage="url(/images/bubbles.svg)"
-      backgroundRepeat="no-repeat"
-      backgroundPosition="bottom right"
+      flex="1"
     >
-
-      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
-        {(propsDoModal) => (
-          <FormCadastro propsDoModal={propsDoModal} />
-          // <Box
-          //   backgroundColor="white"
-          //   {...propsDoModal}
-          // >
-          //   Conteudo 2
-          // </Box>
-        )}
-      </Modal>
-
-      <Menu onCadastrarClick={() => setModalOpen(true)} />
-
-      <Grid.Container>
+      <Grid.Container
+        marginTop={{
+          xs: '32px',
+          md: '75px',
+        }}
+      >
         <Grid.Row>
           <Grid.Col
-            offset={{
-              xs: 0,
-              md: 1,
-            }}
-            value={{
-              xs: 12,
-              md: 5,
-            }}
+            value={{ xs: 12, md: 5 }}
+            offset={{ xs: 0, md: 1 }}
             display="flex"
             alignItems="flex-start"
             justifyContent="center"
@@ -58,6 +34,7 @@ export default function Home() {
               variant="title"
               tag="h1"
               color="tertiary.main"
+          // textAlign="right"
               textAlign={{
                 xs: 'center',
                 md: 'left',
@@ -74,19 +51,18 @@ export default function Home() {
                 md: 'left',
               }}
             >
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
             </Text>
 
             <Button
-              display="block"
               margin={{
                 xs: 'auto',
                 md: 'initial',
               }}
+              display="block"
               variant="primary.main"
-              onClick={() => setModalOpen(!isModalOpen)}
+              onClick={() => websitePageContext.toggleModalCadastro()}
             >
               Cadastrar
             </Button>
@@ -98,15 +74,26 @@ export default function Home() {
             }}
           >
             <img
-              alt="Imagem do site dentro de um celular"
+              alt="Imagem de celular com páginas internas do projeto com o perfil do Cage"
               style={{ display: 'block', margin: 'auto' }}
               src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
             />
           </Grid.Col>
         </Grid.Row>
       </Grid.Container>
-
-      <Footer />
     </Box>
   );
 }
+
+export default websitePageHOC(HomeScreen, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Home',
+    },
+    pageBoxProps: {
+      backgroundImage: 'url(/images/bubbles.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom right',
+    },
+  },
+});
