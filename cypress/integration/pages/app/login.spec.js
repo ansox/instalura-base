fa3de551e9108be67f70fcc5ba853b758e3e53ca
Anso <anderson.floriano@gmail.com>
2022-02-07 09:15:12 -0300

    fix(fix specs lint): fix expecs lint

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,10 +2,10 @@
 
 describe('/pages/app/login', () => {
   it('preenchas os campos e vá para página de perfil', () => {
-    cy.visit('/app/login')
-    cy.get('#formCadastro input[name="usuario"]').type('usuario')
-    cy.get('#formCadastro input[name="senha"]').type('senha')
-    cy.get('#formCadastro button[type="submit"]').click()
-    cy.url().should('include', '/app/profile')
-  })
-})
+    cy.visit('/app/login');
+    cy.get('#formCadastro input[name="usuario"]').type('usuario');
+    cy.get('#formCadastro input[name="senha"]').type('senha');
+    cy.get('#formCadastro button[type="submit"]').click();
+    cy.url().should('include', '/app/profile');
+  });
+});
