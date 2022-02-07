7df6eb80bea5777a0802c8ac45cc03516733f35a
Anso <anderson.floriano@gmail.com>
2022-02-07 09:05:51 -0300

    feat(tests): add cypress tests to the project

---
+++ cypress/integration/pages/app/login.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('/pages/app/login', () => {
+  it('preenchas os campos e vá para página de perfil', () => {
+    cy.visit('/app/login')
+    cy.get('#formCadastro input[name="usuario"]').type('usuario')
+    cy.get('#formCadastro input[name="senha"]').type('senha')
+    cy.get('#formCadastro button[type="submit"]').click()
+    cy.url().should('include', '/app/profile')
+  })
+})
