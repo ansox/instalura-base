3ee9d7251d48ae2002d7248069c495f4fddd7fc7
Anso <anderson.floriano@gmail.com>
2022-02-14 08:32:50 -0300

    feat(form): add form validation

--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-export default function useForm({ initialValues, onSubmit }) {
+export default function useForm({ initialValues, onSubmit, validateSchema }) {
   const [values, setValues] = React.useState(initialValues);
+
+  const [isFormDisabled, setIsFormDisabled] = React.useState(true);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouchedFields] = React.useState({});
+
+  React.useEffect(() => {
+    validateSchema(values)
+      .then(() => {
+        setIsFormDisabled(false);
+        setErrors({});
+      })
+      .catch((error) => {
+        setIsFormDisabled(true);
+
+        const formatedErrors = error.inner.reduce((acc, { path, message }) => ({
+          ...acc,
+          [path]: message,
+        }), {});
+
+        setErrors(formatedErrors);
+      });
+  }, [values]);
 
   return {
     values,
@@ -17,6 +39,17 @@ export default function useForm({ initialValues, onSubmit }) {
         ...currentValues,
         [fieldName]: value,
       }));
+    },
+    isFormDisabled,
+    errors,
+    touched,
+    handleBlur(event) {
+      const fieldName = event.target.getAttribute('name');
+
+      setTouchedFields({
+        ...touched,
+        [fieldName]: true,
+      });
     },
   };
 }
